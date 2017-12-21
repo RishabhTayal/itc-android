756775ea548cfe758a40220674abdb3264bc742a
Rishabh Tayal <rtayal11@gmail.com>
2017-12-21 16:39:50 -0600

    commit

---
+++ app/src/main/java/com/rtayal/itunesconnect/dummy/ReviewItem.java
@@ -0,0 +1,21 @@
+package com.rtayal.itunesconnect.dummy;
+
+import java.util.HashMap;
+
+public class ReviewItem {
+    public final String id;
+    public final String title;
+    public final String rating;
+
+    public ReviewItem(HashMap<String, Object> object) {
+        this.id = object.get("id").toString();
+        this.title = object.get("title").toString();
+        this.rating= object.get("rating").toString();
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+}
